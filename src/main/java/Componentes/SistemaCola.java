9012704b9171bd0ae51f61d3a63e5419a8cf0992
Miguel Valdez <mavd22mv@gmail.com>
2021-01-03 19:50:57 -0400

    Inicio a desarrollo clase SistemaCola

---
+++ src/main/java/Componentes/SistemaCola.java
@@ -0,0 +1,30 @@
+package Componentes;
+
+/**
+ * Gestiona la simulacion de un sistema de cola
+ * 
+ * @version Nº1 02/12/2020
+ * @author Miguel Valdez
+ */
+public class SistemaCola {
+    
+    private String unidadTiempo;
+    private int duracionSimulacion;
+    private int cantClientesPermitidos;
+    private int costoEsperaCliente;
+    private int cantServidores;
+    private int costoServidor;  
+    private TablaDistribucion tablaTELL;
+    private TablaDistribucion tablaTiempoServicio;
+
+    public SistemaCola(String unidadTiempo, int duracionSimulacion, int cantClientesPermitidos, int costoEsperaCliente, int cantServidores, int costoServidor, TablaDistribucion tablaTELL, TablaDistribucion tablaTiempoServicio) {
+        this.unidadTiempo = unidadTiempo;
+        this.duracionSimulacion = duracionSimulacion;
+        this.cantClientesPermitidos = cantClientesPermitidos;
+        this.costoEsperaCliente = costoEsperaCliente;
+        this.cantServidores = cantServidores;
+        this.costoServidor = costoServidor;
+        this.tablaTELL = tablaTELL;
+        this.tablaTiempoServicio = tablaTiempoServicio;
+    }
+}
